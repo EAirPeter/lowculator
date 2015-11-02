3e1e17e58aa19317bd3360920b3207c14293f65b
EAirPeter <VioletCrestfall@hotmail.com>
2015-11-02 20:36:36 +0900

    Scheduling

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-#define UNREF(x_) ((void) (sizeof(x_)))
-#define UNREF_PTR(x_) UNREF((void *) x_)
-
 int main(int argc, char *argv[]) {
-    puts("hello world");
-    UNREF(argc);
-    UNREF_PTR(argv);
+
 }
 
